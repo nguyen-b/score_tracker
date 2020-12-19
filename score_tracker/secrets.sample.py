ec03fd008f2a5b311cfec45db435e48d2e7abdb3
Brandon Nguyen <me@bnguyen.org>
2020-12-18 16:03:04 -0800

    fix: Redact Django SECRET_KEY

---
+++ score_tracker/secrets.sample.py
@@ -0,0 +1,2 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'INSERT_KEY_HERE'
--- score_tracker/settings.py
+++ score_tracker/settings.py
@@ -9,18 +9,19 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from score_tracker import secrets
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bn9de!o%6fny7n8ngf8ad_99sh4py()4r31i7+(rox49d7@ocb'
+SECRET_KEY = secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
