030505e912ccf6374bbb7f024c704bf00fd59cb6
Brandon Nguyen <me@bnguyen.org>
2020-12-20 22:22:46 -0800

    fix: Replace description with verbose_name

--- teams/models/Coach.py
+++ teams/models/Coach.py
@@ -4,10 +4,10 @@ from teams.models.School import School
 
 
 class Coach(models.Model):
-    last_name = models.CharField(max_length=64, description="Last Name")
-    first_name = models.CharField(max_length=64, description="First Name")
-    school = models.ForeignKey(School, on_delete=models.RESTRICT, description="School")
-    email = models.EmailField(description="Email")
+    last_name = models.CharField(max_length=64, verbose_name="Last Name")
+    first_name = models.CharField(max_length=64, verbose_name="First Name")
+    school = models.ForeignKey(School, on_delete=models.RESTRICT, verbose_name="School")
+    email = models.EmailField(verbose_name="Email")
 
     def __str__(self):
         return self.first_name + " " + self.last_name
--- teams/models/Team.py
+++ teams/models/Team.py
@@ -10,10 +10,10 @@ class Team(models.Model):
         MS = "Middle School"
         AS = "All Service"
 
-    team_id = models.CharField(max_length=7, description="Team ID", validators=[RegexValidator(r'\d{2}-\d{4}')])
-    unique_id = models.CharField(max_length=19, description="Unique ID",
+    team_id = models.CharField(max_length=7, verbose_name="Team ID", validators=[RegexValidator(r'\d{2}-\d{4}')])
+    unique_id = models.CharField(max_length=19, verbose_name="Unique ID",
                                  validators=[RegexValidator(r'[A-z0-9]{4}(?:-[A-z0-9]{4}){3}')])
-    coach = models.ForeignKey(Coach, on_delete=models.RESTRICT, description="Coach")
+    coach = models.ForeignKey(Coach, on_delete=models.RESTRICT, verbose_name="Coach")
     division = TeamDivisionChoices.choices
 
     def __str__(self):
