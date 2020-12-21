45c8552deee88eaacd74df5057fec07a8b672424
Brandon Nguyen <me@bnguyen.org>
2020-12-20 23:00:14 -0800

    feat: Add models to admin.py

--- scores/admin.py
+++ scores/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from scores.models import Round
+
+admin.site.register(Round)
--- teams/admin.py
+++ teams/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from teams.models import Coach, School, Team
+
+admin.site.register(Coach)
+admin.site.register(School)
+admin.site.register(Team)
