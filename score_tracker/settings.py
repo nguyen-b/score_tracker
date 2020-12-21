b9788675fd48fc19826320da8e103ffbe93971f9
Brandon Nguyen <me@bnguyen.org>
2020-12-20 22:10:07 -0800

    docs: Add comment about security for SECRET_KEY

--- score_tracker/settings.py
+++ score_tracker/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
