666893a3b0dab9d20a49284f9fefc74b2818581d
Brandon Nguyen <me@bnguyen.org>
2020-12-20 22:21:36 -0800

    fix: Add decimal_places and max_digits to DecimalField

--- scores/models/TeamScore.py
+++ scores/models/TeamScore.py
@@ -8,9 +8,9 @@ from teams.models.Team import Team
 class TeamScore(models.Model):
     team = models.ForeignKey(Team, on_delete=models.PROTECT)
     round = models.ForeignKey(Round, on_delete=models.PROTECT)
-    cumulative_score = models.DecimalField()
-    cisco_score = models.DecimalField()
-    image_score = models.DecimalField()
+    cumulative_score = models.DecimalField(decimal_places=2, max_digits=5)
+    cisco_score = models.DecimalField(decimal_places=2, max_digits=5)
+    image_score = models.DecimalField(decimal_places=2, max_digits=5)
     individual_scores = JSONField(blank=True, null=True)
     total_rank = models.IntegerField()
     state_rank = models.IntegerField()
