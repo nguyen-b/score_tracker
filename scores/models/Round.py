3bde5141fcbf8e27951d4690b0c0b940e325d0a0
Brandon Nguyen <me@bnguyen.org>
2020-12-22 19:03:09 -0800

    feat: Add column names to Round model

--- scores/models/Round.py
+++ scores/models/Round.py
@@ -8,6 +8,12 @@ class Round(models.Model):
     end_date = models.DateField()
     scores_csv = models.FileField()
     individual_challenges = JSONField(blank=True, null=True)
+    col_team = models.CharField(max_length=64)
+    col_division = models.CharField(max_length=64)
+    col_state = models.CharField(max_length=64)
+    col_cum = models.CharField(max_length=64)
+    col_cisco = models.CharField(max_length=64)
+    col_image = models.CharField(max_length=64)
 
     def __str__(self):
         return self.name
