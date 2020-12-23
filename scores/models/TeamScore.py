621876285c66243538a05a6482a5dc8d759dcf8c
Brandon Nguyen <me@bnguyen.org>
2020-12-22 19:13:22 -0800

    feat: Update model TeamScore

--- scores/models/TeamScore.py
+++ scores/models/TeamScore.py
@@ -8,12 +8,12 @@ from teams.models.Team import Team
 class TeamScore(models.Model):
     team = models.ForeignKey(Team, on_delete=models.PROTECT)
     round = models.ForeignKey(Round, on_delete=models.PROTECT)
-    cumulative_score = models.DecimalField(decimal_places=2, max_digits=5)
-    cisco_score = models.DecimalField(decimal_places=2, max_digits=5)
-    image_score = models.DecimalField(decimal_places=2, max_digits=5)
+    cumulative_score = models.DecimalField(decimal_places=2, max_digits=5, null=True)
+    cisco_score = models.DecimalField(decimal_places=2, max_digits=5, null=True)
+    image_score = models.DecimalField(decimal_places=2, max_digits=5, null=True)
     individual_scores = JSONField(blank=True, null=True)
-    total_rank = models.IntegerField()
-    state_rank = models.IntegerField()
+    division_rank = models.IntegerField(null=True)
+    state_rank = models.IntegerField(null=True)
 
     class Meta:
         unique_together = ('team', 'round', )
